e125bfdc1e26ee2c7614c2fd549548cdae8e8e55
isra <israelkouperman@gmail.com>
2020-02-16 01:24:56 +0200

    add build to commit

---
+++ build/precache-manifest.0005197d7a3ec169e6070c556ee9d8c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af6a1b2481ad43fc45776bf92fc7309",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27bae76988b05bbf0b6e",
+    "url": "/static/css/main.01c408cb.chunk.css"
+  },
+  {
+    "revision": "5286691ed4fb26733fa3",
+    "url": "/static/js/2.c68572a5.chunk.js"
+  },
+  {
+    "revision": "f32f3f2739f945f6e8e72f406ee3a70f",
+    "url": "/static/js/2.c68572a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27bae76988b05bbf0b6e",
+    "url": "/static/js/main.dcfd3ce6.chunk.js"
+  },
+  {
+    "revision": "22c4475d22b4cd52eabc",
+    "url": "/static/js/runtime-main.c8f24396.js"
+  }
+]);
